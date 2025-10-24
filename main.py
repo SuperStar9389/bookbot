b349f6095548428daa53d0e3a3449205894828a6
SuperStar9389 <theodoreodysseus@outlook.com>
2025-10-24 19:43:23 -0400

    add advanced display and character count

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_word_count
+from stats import get_word_count, count_letters, organize_dict
 
 def get_book_text(filepath):
 	with open(filepath) as file:
@@ -7,7 +7,12 @@ def get_book_text(filepath):
 def main():
 	book = get_book_text("books/frankenstein.txt")
 	word_count = get_word_count(book)
+	letter_count = organize_dict(count_letters(book))
+	print("============ BOOKBOT ============\nAnalyzing book found at books/frankenstein.txt...\n----------- Word Count ----------")
 	print(f"Found {word_count} total words")
+	print("--------- Character Count -------")
+	for i in letter_count:
+		print(f"{i["char"]}: {i["num"]}")
 
 
 main()
--- stats.py
+++ stats.py
@@ -1,2 +1,21 @@
 def get_word_count(text):
 	return len(text.split())
+def count_letters(text):
+	result = {}
+	for i in text:
+		try:
+			result[i.lower()] += 1
+		except:
+			result[i.lower()] = 1
+	return result
+def sort_by_num(data):
+	return data["num"]
+def organize_dict(data):
+	result = []
+	for i in data.keys():
+		if i == " ": continue
+		result.append({"char": i, "num": data[i]})
+	result.sort(reverse=True, key=sort_by_num)
+	return result
+
+
