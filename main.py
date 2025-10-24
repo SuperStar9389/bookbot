b9e4839e63bc8afbe03a3a0ed8f9f664504c4e86
SuperStar9389 <theodoreodysseus@outlook.com>
2025-10-24 19:49:46 -0400

    add argument support

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from stats import get_word_count, count_letters, organize_dict
+import sys
 
 def get_book_text(filepath):
 	with open(filepath) as file:
 		return file.read()
 
-def main():
-	book = get_book_text("books/frankenstein.txt")
+def main(book_path):
+	book = get_book_text(book_path)
 	word_count = get_word_count(book)
 	letter_count = organize_dict(count_letters(book))
 	print("============ BOOKBOT ============\nAnalyzing book found at books/frankenstein.txt...\n----------- Word Count ----------")
@@ -14,5 +15,7 @@ def main():
 	for i in letter_count:
 		print(f"{i["char"]}: {i["num"]}")
 
-
-main()
+if len(sys.argv) <= 1:
+	print("Usage: python3 main.py <path_to_book>")
+	sys.exit(1)
+main(sys.argv[1])
